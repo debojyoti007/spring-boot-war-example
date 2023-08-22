0243b1b5b03450aec296e493edb3d00fe2b5275e
debojyoti007 <36267397+debojyoti007@users.noreply.github.com>
2023-08-22 22:57:06 +0530

    Update HelloWorldApplication.java
    yuvaraj

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Welcome to my Online Learning Classes - Jenkins pipeline complete by Shrusti";
+        return "Welcome to my Online Learning Classes - Jenkins pipeline complete by Yuvaraj";
     }
 
 }
